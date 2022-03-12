9cb3a47c45014df3482e12dbfde458075b7714c3
LIsrayelyan <amaetp@gmail.com>
2022-03-12 22:11:52 +0200

    create exception recipe, create exception ingredient

---
+++ src/main/java/com/bookcompany/builder/exception/com/bookcompany/builder/exception.java
@@ -0,0 +1,13 @@
+package com.bookcompany.builder.exception.com.bookcompany.builder;
+
+import com.bookcompany.builder.model.Recipe;
+
+public class exception<that, recipe> {
+    String message;
+    String Throwable;
+    String ErrorCode = (String) (code = null);
+    private Object code = null;
+
+}
+
+
---
+++ src/main/java/com/bookcompany/builder/exception/com/bookcompany/builder/exception1.java
@@ -0,0 +1,9 @@
+package com.bookcompany.builder.exception.com.bookcompany.builder;
+
+public class exception1<that, ingredient> {
+    String message;
+    String Throwable;
+    String ErrorCode = (String) (code = null);
+    private Object code = null;
+
+}
